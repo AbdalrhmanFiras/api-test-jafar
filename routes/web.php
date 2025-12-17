c99a545f556f16a2047f7abe8c75c8acb06c176c
Abd AlRhamn Faras <aa3819341@gmail.com>
2025-12-17 17:30:24 +0300

    a

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/', function () {
 Route::get('/test-db', function() {
     try {
         DB::connection()->getPdo();
+        
       
         return 'DB connection OK';
     } catch (\Exception $e
