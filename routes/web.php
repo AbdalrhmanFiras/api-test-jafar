e885f555f6dee9b6087b69b818f14357f5b95fc7
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-01-08 05:42:52 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 
 
@@ -10,7 +11,11 @@ Route::get('/home', function () {
     return view('welcome');
 });
 
+Route::get('/auth/google', function () {
+    return Socialite::driver('google')->redirect();
+});
 
+Route::get('/auth/google/callback', [AuthController::class, 'googleCallback']);
 
 
 Route::get('/test-db', function() {
